07be39cd36fc643443e0ac16d10668c1cfd0de79
pocu46 <andrew.pulyaev@gmail.com>
2021-10-10 17:26:24 +0300

    tests with Contains

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -38,8 +38,28 @@ it('Using Get with Find and Eq', () => {
     cy.get('tbody').find('td').find('div').find('button').eq('0')
 })
 
-it.only('Using Get with Find and Eq', () => {
+it('Using Get with Find and Eq', () => {
     cy.viewport(1800, 700)
     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
     cy.get('aside').find('div').find('ol').find('li').find('a').eq(0)
+})
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in')
+})
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in')
+})
+
+it('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN', {matchCase: false})
+})
+
+it.only('', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('footer').contains('Go to old version')
 })
