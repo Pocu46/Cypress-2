dde625bc60e8b7561dcf220cca73bd78b3812f90
pocu46 <andrew.pulyaev@gmail.com>
2021-10-11 21:17:58 +0300

    tests with Should + And, Expect

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -59,7 +59,51 @@ it('', () => {
     cy.contains('SIGN IN', {matchCase: false})
 })
 
-it.only('', () => {
+it('', () => {
     cy.visit('https://next.privat24.ua/mobile?lang=en')
     cy.get('footer').contains('Go to old version')
+})
+
+it('Should', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100)
+        .should('have.value', 100)
+        .and('be.visible')
+})
+
+it('Expect', () => {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100)
+        .then(input => {
+            expect(input).to.have.value(100)
+        })
+})
+
+it('Check default value for Deposits', () => {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+        .should('be.checked')
+})
+
+it('Check is visible Archive link', () => {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.contains('Мої депозити')
+        .trigger('mouseover')
+        .get('#archiveDeposits')
+        .should('be.visible')
+})
+
+it('Check is correct attr in button', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+        .should('have.attr', 'type')
+        .and('match', /button/)
+})
+
+it.only('Check is correct URL', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.url()
+        .should('eq', 'https://next.privat24.ua/?lang=en')
 })
